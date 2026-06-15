8f858b5426cbd80ef5f739be2e60588ae5f23199
Juan Cruz <juancruzperez933@gmail.com>
2026-06-15 15:33:17 -0300

    fix: Agregado de allowed hots

--- sistema_adopcion.Api/sistema_adopcion_manchitas/settings.py
+++ sistema_adopcion.Api/sistema_adopcion_manchitas/settings.py
@@ -25,8 +25,12 @@ SECRET_KEY = 'django-insecure-1@#^ke0p92%k23w1g)=kq$*=!t-l+!85(e@zuj)zqd3b-qacu0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    'juanchosss.pythonanywhere.com',
+    'www.juanchosss.pythonanywhere.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 # Application definition
 
